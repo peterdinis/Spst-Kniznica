eb4ee15520489a808f7be12ec0a07430ec95f737
Peterdinis <peterdinis0@pobox.sk>
2022-11-27 11:57:29 +0100

    feat: Working on category modue

--- libs/backend-libs/category/src/lib/category.module.ts
+++ libs/backend-libs/category/src/lib/category.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
+import { PrismaService } from '@spst-kniznica-project/backend-libs/database';
 
 @Module({
-  controllers: [],
-  providers: [],
+  controllers: [CategoryController],
+  providers: [CategoryService, PrismaService],
   exports: [],
 })
+
 export class CategoryModule {}
