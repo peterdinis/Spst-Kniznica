4fabe56169fc63212ccedc13eacb1675770521f6
Peterdinis <peterdinis0@pobox.sk>
2022-11-27 11:54:06 +0100

    feat: Start working on new module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from '@spst-kniznica-project/backend-libs/books';
 import { PrismaService } from '@spst-kniznica-project/backend-libs/database';
-
+import { CategoryModule } from '@spst-kniznica-project/backend-libs/category';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [BooksModule, ConfigModule.forRoot({
-    envFilePath: ".env"
-  }),],
+  imports: [
+    CategoryModule,
+    BooksModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- libs/backend-libs/category/src/index.ts
+++ libs/backend-libs/category/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/backend-libs-category.module';
+export * from "./lib/index"
