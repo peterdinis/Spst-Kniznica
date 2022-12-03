4a98ecff752149cafe8634acce1813941be7e968
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 12:08:40 +0100

    feat: New import to booking module

--- libs/backend-libs/quotes/src/lib/qoutes.module.ts
+++ libs/backend-libs/quotes/src/lib/qoutes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@spst-kniznica-project/backend-libs/database';
 import { QuotesController } from './qoutes.controller';
 import { QuotesService } from './qoutes.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [QuotesController],
   providers: [QuotesService],
   exports: [],
