8d52266db3d7fce5ddaa9e9d5b3f5098e7c42c16
Peterdinis <peterdinis0@pobox.sk>
2022-12-09 22:18:17 +0100

    feat: Updating prisma

--- libs/backend-libs/student/src/lib/student.service.ts
+++ libs/backend-libs/student/src/lib/student.service.ts
@@ -1,16 +1,64 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "@spst-kniznica-project/backend-libs/database";
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '@spst-kniznica-project/backend-libs/database';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { CreateNewStudentDto } from './dto/create-student-dto';
+import { Prisma } from '@prisma/client';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class StudentService {
-    constructor(
-        private readonly prismaService: PrismaService,
-        private readonly jwtService: JwtService
-    ) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService
+  ) {}
 
-    async signupStudent() {}
+  async signupStudent(registerDto: CreateNewStudentDto) {
+    try {
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          // unique constraint
+          throw new ConflictException('Student already exists in database');
+        } else throw e;
+      } else throw e;
+    }
+  }
 
-    async loginStudent() {}
+  async loginStudent() {}
+
+  async validateUser(payload: JwtPayload) {
+    const user = (await this.prismaService.student.findUnique({
+      where: { id: payload.id } as any,
+    })) as any; // TODO: Fix this problem
+
+    if (
+      user !== null &&
+      user.email === payload.email &&
+      user.username === payload.username
+    ) {
+      return user;
+    }
+    throw new UnauthorizedException('User is not authorized...');
+  }
+
+  async isUsernameAvailable(username: string): Promise<boolean> {
+    const user = await this.prismaService.student.findUnique({
+      where: { username: username.toLowerCase() },
+      select: { username: true },
+    });
+    return user === null;
+  }
+
+  async isEmailAvailable(email: string): Promise<boolean> {
+    const user = await this.prismaService.student.findUnique({
+      where: { email: email.toLowerCase() },
+      select: { email: true },
+    });
+    return user === null;
+  }
 }
