e1d89b734429417b3bebd53e5b4574c0a52bc598
Peterdinis <peterdinis0@pobox.sk>
2022-12-10 19:30:18 +0100

    feat: Working on homepage design wip

--- libs/frontend-libs/hero/src/index.ts
+++ libs/frontend-libs/hero/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/frontend-libs-hero';
 export * from "./lib/Hero";
 export * from "./lib/Services";
 export * from "./lib/Footer"
---
+++ libs/frontend-libs/hero/src/lib/RandomQoute.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RandomQoute() {
+  return (
+    <div>RandomQoute</div>
+  )
+}
+
+export default RandomQoute
