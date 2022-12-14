f736821742f966e9b1c0e52a4a75aed5c7571a49
Peterdinis <peterdinis0@pobox.sk>
2022-12-14 12:43:23 +0100

    feat: New lib

---
+++ libs/frontend-libs/quotes/src/index.ts
@@ -0,0 +1,5 @@
+import DisplayAllQoutes from "./lib/DisplayAllQoutes";
+
+export {
+    DisplayAllQoutes
+}
---
+++ libs/frontend-libs/quotes/src/lib/DisplayAllQoutes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DisplayAllQoutes() {
+  return (
+    <div>DisplayAllQoutes</div>
+  )
+}
+
+export default DisplayAllQoutes
