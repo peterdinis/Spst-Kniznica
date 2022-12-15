fb54cf10710a00a195aba0a22efcdaeca4ce249a
Peterdinis <peterdinis0@pobox.sk>
2022-12-15 11:49:13 +0100

    FOO

--- libs/frontend-libs/about/src/lib/AboutWrapper.tsx
+++ libs/frontend-libs/about/src/lib/AboutWrapper.tsx
@@ -1,20 +1,22 @@
 import Paper from '@mui/material/Paper';
-import "./AboutWrapper.css";
-import Header from "libs/frontend-libs/shared/src/lib/Header"
+import './AboutWrapper.css';
+import Header from 'libs/frontend-libs/shared/src/lib/Header';
 
 function AboutWrapper() {
-
   return (
     <>
       <Header name="O Stránke" />
       <Paper elevation={3}>
-      <h1 className="ownerName">Správca je ...</h1>
+        <h1 className="ownerName">Správca je ...</h1>
         <p className="helperText">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sequi et nesciunt sed excepturi quaerat, incidunt a architecto molestiae! Saepe iusto temporibus eaque voluptate officiis facere veniam amet praesentium id.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sequi et
+          nesciunt sed excepturi quaerat, incidunt a architecto molestiae! Saepe
+          iusto temporibus eaque voluptate officiis facere veniam amet
+          praesentium id.
         </p>
       </Paper>
     </>
-  )
+  );
 }
 
-export default AboutWrapper
+export default AboutWrapper;
--- libs/frontend-libs/categories/src/lib/frontend-libs-categories.tsx
+++
@@ -1,9 +0,0 @@
-export function FrontendLibsCategories() {
-  return (
-    <div>
-      <h1>Welcome to FrontendLibsCategories!</h1>
-    </div>
-  );
-}
-
-export default FrontendLibsCategories;
