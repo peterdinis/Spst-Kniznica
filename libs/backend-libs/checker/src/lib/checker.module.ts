cee09e9d73aa9e429e219a909bb41da5dfeec9fb
Peterdinis <peterdinis0@pobox.sk>
2022-12-15 12:49:45 +0100

    feat: Adding new customer db check for prisma wip

--- libs/backend-libs/checker/src/lib/checker.module.ts
+++ libs/backend-libs/checker/src/lib/checker.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CheckerController } from './checker.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
+import { PrismaModule } from '@spst-kniznica-project/backend-libs/database';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
+  imports: [TerminusModule, HttpModule, PrismaModule],
   controllers: [CheckerController],
 })
 export class CheckerModule {}
