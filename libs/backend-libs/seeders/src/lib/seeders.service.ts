821a9c1f7dc2e8b31673ccd5248ddf0a3ca1bf86
Peterdinis <peterdinis0@pobox.sk>
2022-12-17 11:55:16 +0100

    feat: Update seeders service

--- libs/backend-libs/seeders/src/lib/seeders.service.ts
+++ libs/backend-libs/seeders/src/lib/seeders.service.ts
@@ -3,15 +3,29 @@ import {PrismaService} from "@spst-kniznica-project/backend-libs/database"
 import { CreateBookDto } from "./dto/book-seeders.dto";
 import { CreateCategoryDto } from "./dto/category-seeders.dto";
 
+
+/* 
+createMany is not supported on SQLite unfortunately: #10710 Documented here:
+After switch to pg test these endpoints
+*/
+
 @Injectable()
 export class SeedersService {
     constructor(private readonly prismaService: PrismaService) {}
 
     async createManyBooks(newBook: CreateBookDto) {
-        
+        const newBooks = await this.prismaService.book.create({
+            data: newBook
+        })
+
+        return newBooks;
     }
 
     async createManyCategories(newCategory: CreateCategoryDto) {
-        
+        const newCategories = await this.prismaService.category.create({
+            data: newCategory
+        })
+
+        return newCategories;
     }
 }
