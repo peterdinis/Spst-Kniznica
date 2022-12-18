1cca7936529978beaccffb7dc04df29c6c1fe8ee
Peterdinis <peterdinis0@pobox.sk>
2022-12-18 17:11:09 +0100

    feat: New page for book detail

--- libs/frontend-libs/booking/src/lib/BorrowModal.tsx
+++ libs/frontend-libs/booking/src/lib/BorrowModal.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import {style} from "./styles";
 
-function BorrowModal() {
-  return (
-    <div>BorrowModal</div>
-  )
+interface IProps {
+  buttonName: string;
+  children?: React.ReactNode;
+  headerName: string;
 }
 
-export default BorrowModal
+export default function BorrowModal({buttonName, headerName, children}: IProps) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>{buttonName}</Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <h3 className="text-2xl font-bold text-black-800 text-center">
+              {headerName}
+            </h3>
+              {children}
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
--- libs/frontend-libs/books/src/lib/GetOneBookDisplay.tsx
+++ libs/frontend-libs/books/src/lib/GetOneBookDisplay.tsx
@@ -1,10 +1,121 @@
-import React from 'react'
+import { FallbackRender, Header, Missing } from '@spst-kniznica-project/frontend-libs/shared'
+import * as api from "libs/frontend-libs/api/src/lib/queries/bookQueries"
+import { useNavigate, useParams } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useQuery } from '@tanstack/react-query';
+import {placeholderBook} from "libs/frontend-libs/data/src/lib/placeholderBook";
+
 
 function GetOneBookDisplay() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { data, isError } = useQuery(
+    ["bookDetail", id],
+    () => api.getOneBook(id as string),
+    {
+      placeholderData: placeholderBook,
+    }
+  );
+
+  const navigateBack = () => {
+    navigate("/books");
+  };
+
+  if (isError) {
+    return (
+      <>
+        <Missing name="Chyba na strane applikácie. Opakujte operáciu neskôr" />
+      </>
+    )
+  }
   return (
     <>
-      PING
-    </>
+    <Header name="Detail knihy" />
+    {data === undefined || null ? (
+      <FallbackRender error="Nastala chyba" />
+    ) : (
+      <section className="mt-2 text-gray-700 body-font overflow-hidden bg-white">
+        <div className="container px-5 py-12 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            {data.image === null ||
+            data.image === undefined ||
+            !data.image ||
+            data.image === "string" ? (
+              <LazyLoadImage
+                alt="No Image"
+                className="lg:w-1/2 w-full object-cover object-center rounded-lg border drop-shadow-md"
+                src="https://bitsofco.de/content/images/2018/12/broken-1.png"
+              />
+            ) : (
+              <LazyLoadImage
+                alt="ecommerce"
+                className="lg:w-1/2 w-full object-cover object-center rounded-lg border drop-shadow-md"
+                src={data.image}
+              />
+            )}
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              {data.title === null ? (
+                <div className="mt-2">
+                  <Missing name="title chýba" />
+                </div>
+              ) : (
+                <div>
+                  <h1 className="text-gray-900 text-4xl title-font font-medium mb-1">
+                    <span className="font-bold">Názov</span>: {data.name}
+                  </h1>
+                </div>
+              )}
+              {data.description === null ? (
+                <div>
+                  <Missing name="Popis chýba" />
+                </div>
+              ) : (
+                <p className="text-2xl mt-3 font-light leading-relaxed  mb-4 text-gray-800">
+                  <span className="font-bold">Krátke info</span>:{" "}
+                  {data.description}
+                </p>
+              )}
+
+              {data.author === null ? (
+                <div>
+                  <Missing name="Author chýba" />
+                </div>
+              ) : (
+                <p className="text-2xl mt-3 font-light leading-relaxed  mb-4 text-gray-800">
+                  <span className="font-bold">Author</span>: {data.author}
+                </p>
+              )}
+              {data.year === null || data.year === "" ? (
+                <div>
+                  <Missing name="Rok vydania chýba" />
+                </div>
+              ) : (
+                <p className="text-2xl mt-3 font-light leading-relaxed  mb-4 text-gray-800">
+                  <span className="font-bold"> Rok</span>: {data.year}
+                </p>
+              )}
+              {data.pages === null ? (
+                <div>
+                  <Missing name="Počet strán chýba" />
+                </div>
+              ) : (
+                <p className="text-2xl mt-3 font-light leading-relaxed  mb-4 text-gray-800">
+                  <span className="font-bold"> Počet strán</span>:{" "}
+                  {data.pages}
+                </p>
+              )}
+              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5" />
+              
+
+              <button className="mt-10 text-xl" onClick={navigateBack}>
+                Späť na knihy
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    )}
+  </>
   )
 }
 
