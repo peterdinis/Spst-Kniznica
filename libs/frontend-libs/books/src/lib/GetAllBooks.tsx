9a38d76a255acbcd4b4b3de13d3358bbaa3c4b9f
Peterdinis <peterdinis0@pobox.sk>
2022-12-18 20:35:38 +0100

    FOO

--- libs/frontend-libs/books/src/lib/GetAllBooks.tsx
+++ libs/frontend-libs/books/src/lib/GetAllBooks.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import { FallbackRender, Header } from '@spst-kniznica-project/frontend-libs/shared';
+import React from 'react';
 import {
-  useQuery,
-  useQueryErrorResetBoundary,
-} from "@tanstack/react-query";
-import { Link } from "react-router-dom";
-import { ErrorBoundary } from "react-error-boundary";
+  FallbackRender,
+  Header,
+} from '@spst-kniznica-project/frontend-libs/shared';
+import { useQuery, useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 /* TODO: Update !! */
-import * as api from "libs/frontend-libs/api/src/lib/queries/bookQueries";
+import * as api from 'libs/frontend-libs/api/src/lib/queries/bookQueries';
 import Button from '@mui/material/Button';
-import {IBook} from "libs/frontend-libs/api/src/lib/interfaces/IBook";
+import { IBook } from 'libs/frontend-libs/api/src/lib/interfaces/IBook';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import ScrollToTop from "libs/frontend-libs/hooks/src/lib/useScroll";
+import ScrollToTop from 'libs/frontend-libs/hooks/src/lib/useScroll';
 
 export function AllBooks() {
   const { reset } = useQueryErrorResetBoundary();
-  const [searchTerm, setSearchTerm] = React.useState<string>("");
+  const [searchTerm, setSearchTerm] = React.useState<string>('');
 
   const valChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
   };
 
-  const { data, isError} = useQuery(["allBooks"], api.getBooks);
+  const { data, isError } = useQuery(['allBooks'], api.getBooks);
 
-  if(isError) {
-    console.log("Add something here")
+  if (isError) {
+    console.log('Add something here');
   }
 
   return (
@@ -46,37 +46,35 @@ export function AllBooks() {
             placeholder="Hľadaj knihu"
           />
         </div>
-        <section>
-        </section>
+        <section></section>
         <div className="grid gap-8 space-x-1 lg:grid-cols-6">
           <>
             {data &&
-              data
-                .map((item: IBook) => {
-                  return (
-                    <>
-                      <div key={item.id} className="px-4 py-4 mt-4 bg-white">
-                        <span>
-                          {item.image === "" ||
-                          item.image === null ||
-                          item.image === undefined ||
-                          item.image === "string" ? (
-                            <LazyLoadImage
-                              alt="Placeholder"
-                              className="h-auto w-full"
-                              src="https://bitsofco.de/content/images/2018/12/broken-1.png"
-                            />
-                          ) : (
-                            <LazyLoadImage
-                              alt="Placeholder"
-                              className="h-auto w-full"
-                              src={item.image}
-                            />
-                          )}
-                        </span>
-                        <h3 className="text-2xl text-center text-gray-800">
-                          
-                          {item.name} - {item.author} 
+              data.map((item: IBook) => {
+                return (
+                  <>
+                    <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+                      <div className="mb-8">
+                        {item.image === '' ||
+                        item.image === null ||
+                        item.image === undefined ||
+                        item.image === 'string' ? (
+                          <LazyLoadImage
+                            alt="Placeholder"
+                            className="h-auto w-full"
+                            src="https://bitsofco.de/content/images/2018/12/broken-1.png"
+                          />
+                        ) : (
+                          <LazyLoadImage
+                            alt="Placeholder"
+                            className="h-auto w-full"
+                            src={item.image}
+                          />
+                        )}
+                      </div>
+                      <div className="text-center">
+                        <h3 className="text-2xl text-gray-800">
+                          {item.name} - {item.author}
                         </h3>
                         <div className="text-center mt-4">
                           <Link
@@ -87,9 +85,10 @@ export function AllBooks() {
                           </Link>
                         </div>
                       </div>
-                    </>
-                  );
-                })}
+                    </div>
+                  </>
+                );
+              })}
           </>
         </div>
         <ScrollToTop />
