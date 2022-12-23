5bbf53afa594a8c0dfa3cc03ae9860d2abf16d15
Peterdinis <peterdinis0@pobox.sk>
2022-12-23 18:53:23 +0100

    feat: First booking endpoints

--- libs/backend-libs/booking/src/lib/booking.controller.ts
+++ libs/backend-libs/booking/src/lib/booking.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
+import { BookingService } from "./booking.service";
+import { ApiOkResponse, ApiOperation } from "@nestjs/swagger";
 
 @Controller("booking")
-export class BookingController {}
+export class BookingController {
+    constructor(private readonly bookingService: BookingService) {}
+
+    @ApiOperation({
+        summary: "Find all borrowed books"
+    })
+    @ApiOkResponse()
+    @Get("/")
+    async allBorrowedBooks() {
+        return this.bookingService.allBorrowedBooks();
+    }
+
+    @ApiOperation({
+        summary: "Find borrowed book by detail"
+    })
+    @ApiOkResponse()
+    @Get("/:id")
+    async findOneBorrowedBook(
+        @Param("id") id: number
+    ) {
+        return this.bookingService.borrowedBookDetail(id);
+    }
+}
--- libs/backend-libs/booking/src/lib/booking.service.ts
+++ libs/backend-libs/booking/src/lib/booking.service.ts
@@ -1,7 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "@spst-kniznica-project/backend-libs/database";
+import { Booking, Book, Student, Teacher } from "@prisma/client";
 
 @Injectable()
 export class BookingService {
     constructor(private readonly prismaService: PrismaService) {}
+
+    async allBorrowedBooks() {
+        const allBooks = await this.prismaService.booking.findMany();
+        return allBooks;
+    }
+
+    async borrowedBookDetail(id: number) {
+        const borrowedBookDetail = await this.prismaService.booking.findUnique({
+            where: {
+                id
+            }
+        })
+
+        if(!borrowedBookDetail) {
+            throw new NotFoundException("Book with this id not found");
+        }
+
+        return borrowedBookDetail;
+    }
+
+    async borrowBook() {}
+
+    async returnBook() {}
+
+    async extendBook() {}
+
+    async displayMyBorrowedBooks() {}
 }
