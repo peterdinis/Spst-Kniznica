84c618d4f24797390cb52b84d68cfb8d5a9fa6fa
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 14:58:03 +0100

    feat: New field to user-table was add

--- libs/backend-libs/users/src/lib/dto/register-users.dto.ts
+++ libs/backend-libs/users/src/lib/dto/register-users.dto.ts
@@ -1,1 +1,29 @@
-export class RegisterUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class RegisterUserDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    username: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    role: string;
+}
