d1a0c98744eb0386ee0ec640954901b855ba5eb1
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 19:41:12 +0100

    FOO

--- prisma/migrations/20221225140552_new_user_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "username" TEXT NOT NULL PRIMARY KEY,
-    "password" TEXT NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-    "role" TEXT NOT NULL
-);
