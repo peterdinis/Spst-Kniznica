66ffdd530825a927e3bd392a934a4fc669d17c33
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 14:31:25 +0100

    feat: Start rewriting authentication

--- libs/backend-libs/users/src/lib/users.service.ts
+++ libs/backend-libs/users/src/lib/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@spst-kniznica-project/backend-libs/database";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async getUser() {}
+
+    async createUser() {}
+
+    async loginUser() {}
+
+    async registerUser() {}
+}
