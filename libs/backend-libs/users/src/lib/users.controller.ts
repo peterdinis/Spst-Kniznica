75989c695ff75667d52fcbe77d56cdfa91d9ae43
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 15:42:49 +0100

    feat: Updating user.controller

--- libs/backend-libs/users/src/lib/users.controller.ts
+++ libs/backend-libs/users/src/lib/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { RegisterUserDto } from "./dto/register-users.dto";
+import { LoginUserDto } from "./dto/login-users.dto";
+import { AuthGuard } from "@nestjs/passport";
+import AuthUser from "./decorators/user.decorator";
+import type { User } from "@prisma/client";
 
+@ApiTags("Application Users")
 @Controller("users")
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @ApiOperation({
+        summary: "Register new user"
+    })
+    @ApiCreatedResponse({
+        type: RegisterUserDto
+    })
+    @Post("/register")
+    async registerUser(
+        @Body() registerUserDto: RegisterUserDto
+    ) {
+        return this.usersService.registerUser(registerUserDto);
+    }
+
+    @ApiOperation({
+        summary: "Login new user"
+    })
+    @ApiCreatedResponse({
+        type: LoginUserDto
+    })
+    @Post("/login")
+    async loginUser(
+        @Body() loginUserDto: LoginUserDto
+    ) {
+        return this.usersService.loginUser(loginUserDto);
+    }
+
+    @ApiOperation({
+        summary: "Profile info"
+    })
+    @ApiOkResponse()
+    @UseGuards(AuthGuard("jwt"))
+    @Get("/profile")
+    loggedUser(
+        @AuthUser() user: User
+    ) {
+        return user;
+    }
+}
--- libs/backend-libs/users/src/lib/users.module.ts
+++ libs/backend-libs/users/src/lib/users.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '@spst-kniznica-project/backend-libs/database';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt-strategy';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtStrategy],
   exports: [UsersService],
 })
 export class UsersModule {}
