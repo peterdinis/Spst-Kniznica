1b136a1148bce928d6558cd812b6dd57af5735c9
Peterdinis <peterdinis0@pobox.sk>
2022-12-28 13:00:40 +0100

    fix: Fixing small bug

--- libs/backend-libs/booking/src/lib/booking.module.ts
+++ libs/backend-libs/booking/src/lib/booking.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 import {PrismaModule} from "@spst-kniznica-project/backend-libs/database"
+import { BooksModule } from '@spst-kniznica-project/backend-libs/books';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, BooksModule],
   controllers: [BookingController],
   providers: [BookingService]
 })
--- libs/backend-libs/booking/src/lib/booking.service.ts
+++ libs/backend-libs/booking/src/lib/booking.service.ts
@@ -1,10 +1,15 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from '@spst-kniznica-project/backend-libs/database';
 import { CreateNewBookingDto } from './dto/create-booking.dto';
+import { BooksService } from '@spst-kniznica-project/backend-libs/books';
+import { NONAVAIABLE } from './utils/book.status';
 
 @Injectable()
 export class BookingService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly bookService: BooksService
+  ) {}
 
   async allBorrowedBooks() {
     const allBooks = await this.prismaService.booking.findMany();
@@ -30,8 +35,12 @@ export class BookingService {
   async borrowBook(bookingDto: CreateNewBookingDto) {
     const newBorrowedBook = await this.prismaService.booking.create({
       data: {
-        ...bookingDto
+        ...bookingDto,
       },
+    });
+
+    await this.bookService.updateBook(bookingDto.bookId, {
+      status: NONAVAIABLE,
     });
 
     return newBorrowedBook;
