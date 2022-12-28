fafbbe429fc37d7beae62cfa34b323ce42a8c241
Peterdinis <peterdinis0@pobox.sk>
2022-12-28 20:26:57 +0100

    feat: Adding new components wip

--- libs/frontend-libs/pages/src/lib/teacher/TeacherProfile.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function TeacherProfile() {
-  return (
-    <div>TeacherProfile</div>
-  )
-}
-
-export default TeacherProfile
---
+++ libs/frontend-libs/pages/src/lib/users/teacher/TeacherLoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TeacherLoginPage() {
+  return (
+    <div>TeacherLoginPage</div>
+  )
+}
+
+export default TeacherLoginPage
