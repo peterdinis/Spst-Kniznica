cd31389a393db83daf24e7a5df04bf9725695081
Peterdinis <peterdinis0@pobox.sk>
2022-12-29 16:29:45 +0100

    feat: Updating our test endpoint

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Controller, Get } from "@nestjs/common";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AppService } from "./app.service";
 
 @ApiTags("Example")
 @Controller("app")
 export class AppController {
     constructor(private readonly appService: AppService) {}
+
+    @ApiOperation({
+        summary: "Testing endpoint"
+    })
+    @ApiOkResponse()
+    @Get("/example")
+    async getExample() {
+        return await this.appService.exampleData();
+    }
 }
--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,11 +1,20 @@
 import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
+import { AxiosError } from "@nestjs/terminus/dist/errors/axios.error";
+import { catchError, firstValueFrom} from 'rxjs';
 
 @Injectable()
 export class AppService {
     constructor(private readonly httpService: HttpService) {}
 
     async exampleData() {
-        return await this.httpService.get("https://jsonplaceholder.typicode.com/users")
+        const { data } = await firstValueFrom(
+            this.httpService.get('https://jsonplaceholder.typicode.com/users').pipe(
+              catchError((error: AxiosError) => {
+                throw error.message;
+              }),
+            ),
+          );
+          return data;
     }
 }
