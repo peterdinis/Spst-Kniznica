94dde40b86804ed9c1197a59551b186a14f4b8ca
Peterdinis <peterdinis0@pobox.sk>
2022-12-29 16:45:28 +0100

    refactor: Removed one module

--- libs/backend-libs/cache/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/api-cache.module';
-export * from "./lib/api-cache.service"
--- libs/backend-libs/cache/src/lib/api-cache.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApiCachceService } from './api-cache.service';
-
-@Module({
-  providers: [ApiCachceService],
-  exports: [ApiCachceService]
-})
-export class ApiCachceModule {}
