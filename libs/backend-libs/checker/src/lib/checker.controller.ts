d2365a2fd90c3a465c2a5c0dc6c92708643f7008
Peterdinis <peterdinis0@pobox.sk>
2022-12-30 13:32:32 +0100

    refactor: Updating code in checker lib

--- libs/backend-libs/checker/src/lib/checker.controller.ts
+++ libs/backend-libs/checker/src/lib/checker.controller.ts
@@ -1,21 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import {
-  HealthCheckService,
-  HttpHealthIndicator,
   HealthCheck,
-  MemoryHealthIndicator,
 } from '@nestjs/terminus';
-import { PrismaHealthIndicator } from './prisma-check.service';
+import { CheckerService } from './checker.service';
 
 @ApiTags('Checkers')
 @Controller('checker')
 export class CheckerController {
   constructor(
-    private health: HealthCheckService,
-    private http: HttpHealthIndicator,
-    private db: PrismaHealthIndicator,
-    private memory: MemoryHealthIndicator
+    private readonly checkerService: CheckerService
   ) {}
 
   @ApiOperation({
@@ -23,7 +17,7 @@ export class CheckerController {
   })
   @Get('database')
   checkDB() {
-    return this.health.check([() => this.db.isHealthy('database')]);
+    return this.checkerService.checkDatabaseStatus();
   }
 
   @ApiOperation({
@@ -31,9 +25,7 @@ export class CheckerController {
   })
   @Get("spst")
   checkSpst() {
-    return this.health.check([
-      () => this.http.pingCheck('spst-website', 'https://www.spsbj.sk/'),
-    ]);
+    return this.checkerService.checkSpstWebsite();
   }
 
   @ApiOperation({
@@ -42,8 +34,6 @@ export class CheckerController {
   @Get("memory")
   @HealthCheck()
   memoryCheck() {
-    return this.health.check([
-      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
-    ]);
+    return this.checkerService.memoryCheck();
   }
 }
---
+++ libs/backend-libs/checker/src/lib/checker.service.ts
@@ -0,0 +1,34 @@
+import {
+  HealthCheckService,
+  HttpHealthIndicator,
+  HealthCheck,
+  MemoryHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaHealthIndicator } from './prisma-check.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CheckerService {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private db: PrismaHealthIndicator,
+    private memory: MemoryHealthIndicator
+  ) {}
+
+  async checkDatabaseStatus() {
+    return this.health.check([() => this.db.isHealthy('database')]);
+  }
+
+  async checkSpstWebsite() {
+    return this.health.check([
+      () => this.http.pingCheck('spst-website', 'https://www.spsbj.sk/'),
+    ]);
+  }
+
+  async memoryCheck() {
+    return this.health.check([
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+    ]);
+  }
+}
