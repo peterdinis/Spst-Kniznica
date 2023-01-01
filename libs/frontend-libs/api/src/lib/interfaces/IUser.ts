b629a7b1e62bbf6602cb55afd49f847371894047
Peterdinis <peterdinis0@pobox.sk>
2023-01-01 12:18:10 +0100

    feat: Updating api lib

--- libs/frontend-libs/api/src/lib/interfaces/IUser.ts
+++ libs/frontend-libs/api/src/lib/interfaces/IUser.ts
@@ -1,16 +1,27 @@
 export interface IUser {
-    id?: number;
-    username: string;
-    password: string;
-    firstname: string;
-    lastname: string;
-    email: string;
-    isTeacher?: boolean;
-    isStudent?: boolean;
-    borrowedBooks?: any; // later Booking[]  
+  id?: number;
+  username: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  isTeacher?: boolean;
+  isStudent?: boolean;
+  borrowedBooks?: any; // later Booking[]
 }
 
-export interface IRegisterUser {}
+export type IRegisterUser = Pick<
+  IUser,
+  | 'email'
+  | 'username'
+  | 'password'
+  | 'firstname'
+  | 'lastname'
+  | 'email'
+  | 'isTeacher'
+  | 'isStudent'
+>;
 
+export type ILoginUser = Pick<IUser, 'username' | 'email'>;
 
-export interface ILoginUser {}
+export type IUpdateUser = Partial<IUser>;
--- libs/frontend-libs/api/src/lib/mutations/userMutations.ts
+++ libs/frontend-libs/api/src/lib/mutations/userMutations.ts
@@ -1,9 +1,23 @@
 import axios from "axios";
+import { ILoginUser, IRegisterUser, IUpdateUser } from "../interfaces/IUser";
 
 const api = axios.create({
     baseURL:"http://localhost:3333/api/"
 });
 
-export const registerUser = () => {}
+api.defaults.headers.common['Content-Type'] = 'application/json';
 
-export const loginUser = () => {}
+export const registerUser = async (registerUser: IRegisterUser) => {
+ const {data}= await api.post("users/register", registerUser);
+ return data;
+}
+
+export const loginUser = async (loginUser: ILoginUser) => {
+    const {data}= await api.post("users/login", loginUser);
+    return data;
+}
+
+export const updateUserProfile = async(id: number, updateUser: IUpdateUser) => {
+  const {data} = await api.put(`users/${id}/update`, updateUser);
+  return data;
+}
