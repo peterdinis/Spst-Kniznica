1e2a169620020f2128f7808824f8f15114cd7211
Peterdinis <peterdinis0@pobox.sk>
2023-01-04 18:09:37 +0100

    FOO

--- libs/backend-libs/questions/src/lib/questions.controller.ts
+++ libs/backend-libs/questions/src/lib/questions.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { QuestionsService } from "./questions.service";
 import { ViewQuestionDto } from "./dto/view-question.dto";
+import { CreateQuestionDto } from "./dto/create-question.dto";
+import { UpdateQuestionDto } from "./dto/update-question.dto";
+import { AnswerTheQuestionDto } from "./dto/answer-question.dto";
 
 @ApiTags("Questions")
 @Controller("questions")
@@ -17,5 +20,43 @@ export class QuestionsController {
     @Get("/")
     async allQuestions() {
         return await this.questionService.allQuestions();
+    }
+
+    @ApiOperation({
+        summary: "Create new question"
+    })
+    @ApiCreatedResponse({
+        type: CreateQuestionDto
+    })
+    @Post("/")
+    async createNewQuestion(@Body() questionDto: CreateQuestionDto) {
+        return await this.questionService.createNewQuestion(questionDto)
+    }
+
+    @ApiOperation({
+        summary: "Update question"
+    })
+    @ApiOkResponse()
+    @Put("/:id/update")
+    async updateQuestion(@Param("questionId") questionId: number, @Body() updateQuestion: UpdateQuestionDto) {
+        return await this.questionService.updateQuestion(questionId, updateQuestion);
+    }
+
+    @ApiOperation({
+        summary: "Answer question"
+    })
+    @ApiOkResponse()
+    @Put("/:id/question/answer")
+    async answerQuestion(@Param("questionId") questionId: number, @Body() answerQuestion: AnswerTheQuestionDto) {
+        return await this.questionService.answerQuestion(questionId, answerQuestion);
+    }
+
+    @ApiOperation({
+        summary: "Delete question"
+    })
+    @ApiOkResponse()
+    @Delete("/:id/delete")
+    async deleteQuestion(@Param("questionId") questionId: number) {
+        return await this.questionService.removeQuestion(questionId);
     }
 }
--- libs/backend-libs/questions/src/lib/questions.service.ts
+++ libs/backend-libs/questions/src/lib/questions.service.ts
@@ -1,54 +1,74 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "@spst-kniznica-project/backend-libs/database";
-import { CreateQuestionDto } from "./dto/create-question.dto";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@spst-kniznica-project/backend-libs/database';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { AnswerTheQuestionDto } from './dto/answer-question.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Injectable()
 export class QuestionsService {
-    constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-    async allQuestions() {
-        const allQuestions = await this.prismaService.question.findMany();
-        return allQuestions;
-    }
+  async allQuestions() {
+    const allQuestions = await this.prismaService.question.findMany();
+    return allQuestions;
+  }
 
-    async createNewQuestion(questionDto: CreateQuestionDto) {
-        const newQuestion = await this.prismaService.question.create({
-            data: {
-                name: questionDto.name,
-                answer: questionDto.answer
-            }
-        })
+  async createNewQuestion(questionDto: CreateQuestionDto) {
+    const newQuestion = await this.prismaService.question.create({
+      data: {
+        name: questionDto.name,
+        answer: questionDto.answer,
+      },
+    });
 
-        return newQuestion;
-    }
+    return newQuestion;
+  }
 
-    async answerQuestion(questionId: number, newAnswer: string) {
-        const oneQuestion = await this.prismaService.question.findFirst({
-            where: {
-                id: questionId
-            }
-        })
+  async updateQuestion(questionId: number, questionDto: UpdateQuestionDto) {
+    const oneQuestion = await this.prismaService.question.findFirst({
+      where: {
+        id: questionId,
+      }
+    })
 
-        const answerTheQuestion = await this.prismaService.question.update({ 
-            where: {
-                id: oneQuestion.id
-            },
+    const updatingQuestion = await this.prismaService.question.update({
+      where: {
+        id: oneQuestion.id,
+      },
 
-            data: {
-                answer: newAnswer
-            }
-        })
+      data: questionDto
+    })
 
-        return answerTheQuestion;
-    }
+    return updatingQuestion;
+  }
 
-    async removeQuestion(questionId: number) {
-        const questionToRemoved = await this.prismaService.question.delete({
-            where: {
-                id: questionId
-            }
-        })
+  async answerQuestion(questionId: number,answerQuestionDto: AnswerTheQuestionDto) {
+    const oneQuestion = await this.prismaService.question.findFirst({
+      where: {
+        id: questionId,
+      },
+    });
 
-        return questionToRemoved;
-    }
+    const answerTheQuestion = await this.prismaService.question.update({
+      where: {
+        id: oneQuestion.id,
+      },
+
+      data: {
+        answer: answerQuestionDto.newAnswer,
+      },
+    });
+
+    return answerTheQuestion;
+  }
+
+  async removeQuestion(questionId: number) {
+    const questionToRemoved = await this.prismaService.question.delete({
+      where: {
+        id: questionId,
+      },
+    });
+
+    return questionToRemoved;
+  }
 }
