272fb2a66c7c576e14d5d75455b616a8c244e69d
Peterdinis <peterdinis0@pobox.sk>
2023-01-07 18:23:01 +0100

    feat: Start working on booking in FE wip

--- libs/frontend-libs/api/src/lib/queries/bookingQueries.ts
+++ libs/frontend-libs/api/src/lib/queries/bookingQueries.ts
@@ -1,13 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/api/"
+  baseURL: 'http://localhost:3333/api/',
 });
 
 export const searchingForUser = (username: string) => {
-    if(!username) {
-      return;
-    }
-  
-    return api.get(`booking/myBorrowedBooks?username=${username}`).then((res) => res.data);
+  if (!username) {
+    return;
   }
+
+  return api
+    .get(`booking/myBorrowedBooks?username=${username}`)
+    .then((res) => res.data);
+};
