a6ab13dd086a43d55ef5df864e8767cb4dab2a65
Peterdinis <peterdinis0@pobox.sk>
2023-01-07 15:19:37 +0100

    FOO

---
+++ libs/frontend-libs/books/src/lib/AutoSuggestionBooks.tsx
@@ -0,0 +1,11 @@
+import { ChangeEvent, ChangeEventHandler, FC, useEffect, useState } from "react";
+
+const AutoSuggestionBooks: FC = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default AutoSuggestionBooks;
---
+++ libs/frontend-libs/categories/src/lib/AutoSuggestionCategories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AutoSuggestionCategories() {
+  return (
+    <div>AutoSuggestionCategories</div>
+  )
+}
+
+export default AutoSuggestionCategories
