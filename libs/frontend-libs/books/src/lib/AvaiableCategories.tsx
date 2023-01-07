85a2334045bbec34902bea7dcabde3a169f69af5
Peterdinis <peterdinis0@pobox.sk>
2023-01-07 19:36:30 +0100

    feat: Start updating book lib wip

--- libs/frontend-libs/books/src/lib/AvaiableCategories.tsx
+++ libs/frontend-libs/books/src/lib/AvaiableCategories.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import {style} from "./styles";
 
-function AvaiableCategories() {
-  return (
-    <>
-    ping
-    </>
-  )
+interface ICategoryModalProps {
+  children?: React.ReactNode;
+  buttonName: string;
+  modalHeader: string;
 }
 
-export default AvaiableCategories
+export default function AvaiableCategories(props: ICategoryModalProps) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
+    return (
+      <>
+        <Button onClick={handleOpen}>{props.buttonName}</Button>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <Typography id="transition-modal-title" variant="h6" component="h2">
+                {props.modalHeader}
+              </Typography>
+              {props.children}
+            </Box>
+          </Fade>
+        </Modal>
+      </>
+    );
+  }
