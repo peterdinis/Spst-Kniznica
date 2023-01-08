6124cf7d1f82603466b164061c908ee5ebe6235f
Peterdinis <peterdinis0@pobox.sk>
2023-01-08 11:17:41 +0100

    feat: Updating admin lib

--- libs/backend-libs/admin/src/lib/admin.module.ts
+++ libs/backend-libs/admin/src/lib/admin.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { PrismaModule } from '@spst-kniznica-project/backend-libs/database';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
--- libs/backend-libs/admin/src/lib/admin.service.ts
+++ libs/backend-libs/admin/src/lib/admin.service.ts
@@ -1,7 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "@spst-kniznica-project/backend-libs/database";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '@spst-kniznica-project/backend-libs/database';
 
 @Injectable()
 export class AdminService {
-    constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async getAdmin(username: string) {
+    const admin = await this.prismaService.admin.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!admin) {
+      throw new NotFoundException('Admin not found');
+    }
+
+    delete admin.password;
+    return admin;
+  }
 }
